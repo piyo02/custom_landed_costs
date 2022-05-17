9f312c9228abf14ad6cb75c25f80c73d2a12dee7
piyo02 <alzidni2000@gmail.com>
2022-05-18 00:27:56 +0800

    fix bug cost per product sama dengan total cost

--- models/stock_quant.py
+++ models/stock_quant.py
@@ -19,19 +19,33 @@ class Quant(models.Model):
         if len(self.history_ids) > 1:
             for hist_id in self.history_ids:
                 picking_name = hist_id.picking_id.name
-                landed_costs = self.env['custom.landed.cost'].search([('picking_ids.name', '=', picking_name)])
+                custom_landed_costs = self.env['custom.landed.cost'].search([('picking_ids.name', '=', picking_name)])
                 
-                for landed_cost in landed_costs:
-                    for line in landed_cost.valuation_adjustment_lines:
-                        if line.product_id.name == self.product_id.name:
-                            total_ship_cost += line.additional_landed_cost/line.quantity
+                if custom_landed_costs:
+                    for landed_cost in custom_landed_costs:
+                        for line in landed_cost.valuation_adjustment_lines:
+                            if line.product_id.name == self.product_id.name:
+                                total_ship_cost += line.additional_landed_cost/line.quantity
+                else:
+                    landed_costs = self.env['stock.landed.cost'].search([('picking_ids.name', '=', picking_name)])
+                    for landed_cost in landed_costs:
+                        for line in landed_cost.valuation_adjustment_lines:
+                            if line.product_id.name == self.product_id.name:
+                                total_ship_cost += line.additional_landed_cost/line.quantity
+
         else:
             picking_name = self.history_ids.picking_id.name
-            landed_cost = self.env['custom.landed.cost'].search([('picking_ids.name', '=', picking_name)])
+            custom_landed_costs = self.env['custom.landed.cost'].search([('picking_ids.name', '=', picking_name)])
 
-            for line in landed_cost.valuation_adjustment_lines:
-                if line.product_id.name == self.product_id.name:
-                    total_ship_cost += line.additional_landed_cost/line.quantity
+            if custom_landed_costs:
+                for line in custom_landed_costs.valuation_adjustment_lines:
+                    if line.product_id.name == self.product_id.name:
+                        total_ship_cost += line.additional_landed_cost/line.quantity
+            else:
+                landed_costs = self.env['stock.landed.cost'].search([('picking_ids.name', '=', picking_name)])
+                for line in landed_costs.valuation_adjustment_lines:
+                    if line.product_id.name == self.product_id.name:
+                        total_ship_cost += line.additional_landed_cost/line.quantity
             
         if total_ship_cost > 0:
             self.ship_cost = total_ship_cost
